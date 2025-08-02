d218a94b42e7c04b2ce5d572fbe28ccc28ad93d8
Vansh Gupta <vanshgupta6839@smail.com>
2025-08-02 14:28:11 +0530

    Fixxed model and added search.html

--- app.py
+++ app.py
@@ -8,31 +8,32 @@ db = SQLAlchemy(app)
 
 class Product(db.Model):
     Sno = db.Column(db.Integer, primary_key=True)
-    product = db.Column(db.String(200), nullable=False)
+    product = db.Column(db.String(100), nullable=False)
+    varity = db.Column(db.String(100), nullable=True)
     desc = db.Column(db.String(200), nullable=True)
     rate = db.Column(db.Integer, nullable=True)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         product = request.form['product']
         new_product = Product(product=product)
         db.session.add(new_product)
         db.session.commit()
-    
+
     all_products = Product.query.all()
     return render_template("index.html", all_products=all_products)
 
-@app.route("/search")
+@app.route('/search')
 def search():
-    query = request.args.get("query")
+    query = request.args.get('query')
     if query:
         results = Product.query.filter(Product.product.ilike(f"%{query}%")).all()
     else:
         results = []
     return render_template("search.html", search_results=results, query=query)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
